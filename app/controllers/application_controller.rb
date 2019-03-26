0e7d7db9b4a1ec52af90e5fa4bfc0244768aca29
Your Name <you@example.com>
2019-03-26 13:55:17 -0500

    user delete correction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,9 @@ class ApplicationController < ActionController::Base
 
 
     def current_user
+        if User.where(id:session[:user_id]).count <1
+            session.delete(:user_id)
+        end
         User.find(session[:user_id]) if session[:user_id]
     end
     helper_method :current_user
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,6 +21,7 @@ class SessionsController < ApplicationController
 
 
   def destroy
+  
     session.delete(:user_id) if session[:user_id]
     redirect_to session[:prev_url] and return
   end
