d5afe37536a5a10a1e64dd9c73c455003fec6dae
Your Name <you@example.com>
2019-03-26 12:53:14 -0500

    user & tournament model validations

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,4 +1,6 @@
 class Tournament < ApplicationRecord
+  validates :name, presence: true, length: { in: 2..20 }
+
   belongs_to :user
   has_many :combatants, dependent: :destroy
   has_many :rounds, dependent: :destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+  validates :name, presence: true, length: { in: 2..20 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+
   has_many :combatants, dependent: :destroy
   has_many :points_as_contender, through: :combatants, :source => :points_as_contender
   has_many :points_as_challenger, through: :combatants, :source => :points_as_challenger
